52bcb2e1bbc08547b7ede4078791cb41c69bfd4d
igor.meier@gmail.com <igor@Igors-MBP.lan>
2023-11-19 19:40:32 +0000

    refactory

---
+++ src/Standard.ToList.Model/Constants/RegexPatterns.cs
@@ -0,0 +1,10 @@
+﻿namespace Standard.ToList.Model.Constants
+{
+    public static class RegexPatterns
+    {
+        public static string SEARCHER_AUCHAN_MATCHES =  "<div class=\"auc-product-tile__[name|prices]*\"[^>]*>([\\s\\S]*?)<\\/div>|<span class=\"sales\">([\\s\\S]*?)<\\/span>";
+        public static string SEARCHER_AUCHAN_NAME = "<a class=\"link\"[^>]*>([\\s\\S]*?)<\\/a>";
+        public static string SEARCHER_AUCHAN_PRICE = "<span class=\"value\"[^>]*content=\"(\\d*.\\d*?)\">";
+        public static string SEARCHER_AUCHAN_OPEN_SYMBOLS = "^(?:.*?\\[(?!.*?\\])[^\\]]*|[^[\\r\n]*\\].*?|.*?\\((?!.*?\\))[^)]*|[^(\r\n]*\\).*?)$";
+    }
+}
